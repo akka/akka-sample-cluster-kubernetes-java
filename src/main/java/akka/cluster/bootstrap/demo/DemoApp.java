d3c1aa829f373cfde65ff21ee846c8174f9afcef
Arnout Engelen <arnouten@bzzt.net>
2018-10-26 20:54:02 +0200

    Add readiness check

--- src/main/java/akka/cluster/bootstrap/demo/DemoApp.java
+++ src/main/java/akka/cluster/bootstrap/demo/DemoApp.java
@@ -7,9 +7,12 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.cluster.Cluster;
 import akka.cluster.ClusterEvent;
+import akka.cluster.MemberStatus;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.StatusCodes;
 import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
 import akka.management.AkkaManagement;
 import akka.management.cluster.bootstrap.ClusterBootstrap;
 import akka.stream.ActorMaterializer;
@@ -33,7 +36,14 @@ public class DemoApp extends AllDirectives {
     cluster
       .subscribe(system.actorOf(Props.create(ClusterWatcher.class)), ClusterEvent.initialStateAsEvents(), ClusterEvent.ClusterDomainEvent.class);
 
-    Http.get(system).bindAndHandle(complete("Hello world").flow(system, mat), ConnectHttp.toHost("0.0.0.0", 8080), mat);
+    Route route = path("ready", () -> {
+      if (cluster.selfMember().status() == MemberStatus.up()) {
+        return complete(StatusCodes.OK);
+      } else {
+        return complete(StatusCodes.INTERNAL_SERVER_ERROR);
+      }
+    });
+    Http.get(system).bindAndHandle(route.flow(system, mat), ConnectHttp.toHost("0.0.0.0", 8080), mat);
 
     cluster.registerOnMemberUp(() -> {
       system.log().info("Cluster member is up!");
