008822e871816eb125011f28041a3f3c252a4a38
Christopher Batey <christopher.batey@gmail.com>
2019-11-04 15:49:53 +0000

    Convert to 2.6/typed

--- src/main/java/akka/cluster/bootstrap/demo/DemoApp.java
+++ src/main/java/akka/cluster/bootstrap/demo/DemoApp.java
@@ -3,45 +3,53 @@
  */
 package akka.cluster.bootstrap.demo;
 
-import akka.actor.ActorSystem;
 import akka.actor.Props;
-import akka.cluster.Cluster;
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.ActorSystem;
+import akka.actor.typed.javadsl.Adapter;
+import akka.actor.typed.javadsl.Behaviors;
 import akka.cluster.ClusterEvent;
+import akka.cluster.typed.Cluster;
+import akka.cluster.typed.ClusterCommand;
+import akka.cluster.typed.Subscribe;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.server.AllDirectives;
 import akka.management.scaladsl.AkkaManagement;
 import akka.management.cluster.bootstrap.ClusterBootstrap;
-import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
 
 public class DemoApp extends AllDirectives {
 
   DemoApp() {
-    ActorSystem system = ActorSystem.create("Appka");
 
-    Materializer mat = ActorMaterializer.create(system);
-    Cluster cluster = Cluster.get(system);
+    ActorSystem.create(Behaviors.setup(context -> {
+      akka.actor.ActorSystem classicSystem = Adapter.toClassic(context.getSystem());
+      Cluster cluster = Cluster.get(context.getSystem());
+      context.getLog().info("Started [" + context.getSystem() + "], cluster.selfAddress = " + cluster.selfMember().address() + ")");
+      Materializer mat = Materializer.matFromSystem(classicSystem);
 
-    system.log().info("Started [" + system + "], cluster.selfAddress = " + cluster.selfAddress() + ")");
+      Http.get(classicSystem).bindAndHandle(complete("Hello world").flow(classicSystem, mat), ConnectHttp.toHost("0.0.0.0", 8080), mat);
 
-    //#start-akka-management
-    AkkaManagement.get(system).start();
-    //#start-akka-management
-    ClusterBootstrap.get(system).start();
+      ActorRef<ClusterEvent.MemberEvent> listener = context.spawn(Behaviors.receiveMessage(event -> {
+         context.getLog().info("MemberEvent: {}", event);
+        return Behaviors.same();
+      }), "listener");
 
-    cluster
-      .subscribe(system.actorOf(Props.create(ClusterWatcher.class)), ClusterEvent.initialStateAsEvents(), ClusterEvent.ClusterDomainEvent.class);
+      cluster.subscriptions().tell(new Subscribe<>(listener, ClusterEvent.MemberEvent.class));
 
-    Http.get(system).bindAndHandle(complete("Hello world").flow(system, mat), ConnectHttp.toHost("0.0.0.0", 8080), mat);
+      //#start-akka-management
+      AkkaManagement.get(classicSystem).start();
+      //#start-akka-management
+      ClusterBootstrap.get(classicSystem).start();
 
-    cluster.registerOnMemberUp(() -> {
-      system.log().info("Cluster member is up!");
-    });
+      return Behaviors.empty();
+    }), "Appka");
   }
 
   public static void main(String[] args) {
     new DemoApp();
   }
+
 }
 
